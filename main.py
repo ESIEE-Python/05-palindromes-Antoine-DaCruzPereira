bb8c2c3b23c2cca27527a9cd200028dbe41bf211
Antoine-DaCruzPereira <antoine.dacruzpereira@edu.esiee.fr>
2024-09-20 09:19:16 +0000

    code sur les palindromes

--- main.py
+++ main.py
@@ -1,19 +1,35 @@
+"""
+Aucun module utilisé.
+"""
 #### Fonction secondaire
-
-
+CARAC_SUP = "ABCDEFGHIJKLMNOPQRSTUVWXYZÂÊÎÔÛÄËÏÖÜÀÇÉÈÙâêîôûäëïöüàçéèù',;:!.?-_"
+CARAC_REPLACE = "abcdefghijklmnopqrstuvwxyzaeiouaeiouaceeuaeiouaeiouaceeu         "
 def ispalindrome(p):
+    """
+    retourne sir la chaine de caractère est un palindrome ou non.
 
+    arg: string object p.
+
+    return: boolean object True or False.
+    """
     # votre code ici
-    
-    return False
+    tr_table = str.maketrans(CARAC_SUP, CARAC_REPLACE)
+    pt = p.translate(tr_table)
+    pt = pt.replace(" ", "")
+    pale = pt[::-1]
+    return pt == pale
 
 #### Fonction principale
 
-
 def main():
+    """
+    Test la fonction ispalindrome.
 
+    arg: None.
+
+    return: None.
+    """
     # vos appels à la fonction secondaire ici
-
     for s in ["radar", "kayak", "level", "rotor", "civique", "deifie"]:
         print(s, ispalindrome(s))
 
